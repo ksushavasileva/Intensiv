71504509c2bf4061e1156e15088478b5c0faa3d3
ksenia <ksenia-vasileva05@mail.ru>
2023-08-28 12:18:53 +0300

    ex_23

--- Day_5/ex_2.py
+++ Day_5/ex_2.py
@@ -1,1 +1,16 @@
-a = 
+import random
+a = []
+n = random.randint(1, 10)
+for i in range(n):
+    a.append(random.randint(1, 10))
+print(a)
+print(len(a))
+print(a[-1])
+t = a[-1::-1]
+print(t)
+if (55 in a) and (1717 in a):
+    print("yes")
+else: print("no")
+a.pop(0)
+a.pop(-1)
+print(a)
---
+++ Day_5/ex_3.py
@@ -0,0 +1,6 @@
+n = int(input())
+a = []
+for i in range(n):
+    s = input()
+    a.append(s)
+print(a)
